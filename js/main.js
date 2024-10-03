b215d2fd30fac6f46a2ba1ebec42ed243a613119
Maryam <mariadatey@gmail.com>
2024-10-03 18:21:36 +0530

    First commit`

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Typed.js setup for animation
+
+var typed = new Typed("#element", {
+  strings: ["Front-End Developer.", "Web Desinger."],
+  typeSpeed: 40,
+});
+
+// JavaScript to handle button click and redirect to GitHub account
+
+document.getElementById("btn").addEventListener("click", function () {
+  window.open("https://github.com/MARIA-DATEY", "blank");
+});
